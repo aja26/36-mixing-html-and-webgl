5a7da0be4c243e42d1e68235feb96619716938c1
Adam Alexander <46898882+aja26@users.noreply.github.com>
2024-02-06 23:59:39 +0000

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
-import { gsap } from 'gsap'
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { gsap } from 'gsap';
 
 /**
  * Loaders
